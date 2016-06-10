1ef43a735689466d3b1b5f8a206b038e75159d2b
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2016-06-10 16:46:54 +0200

    Include Git hash in version message (fix #2023)

--- src/full/Agda/Version.hs
+++ src/full/Agda/Version.hs
@@ -1,12 +1,25 @@
+{-# LANGUAGE TemplateHaskell #-}
 module Agda.Version where
 
+import Data.Maybe (maybe)
 import Data.Version
 import Data.List
 
+import Distribution.VcsRevision.Git
+import Language.Haskell.TH.Syntax
 import qualified Paths_Agda as PA
 
 -- | The version of Agda.
 
+gitVersion :: Maybe String
+gitVersion = $(do
+  v <- qRunIO getRevision
+  lift $ case v of
+    Nothing           -> Nothing
+    Just (hash,True)  -> Just $ hash ++ " (with local modifications)"
+    Just (hash,False) -> Just $ hash)
+
 version :: String
-version = intercalate "." $ map show $
-            versionBranch PA.version
+version = (intercalate "." $ map show $
+            versionBranch PA.version) ++
+          maybe "" (". Git commit: " ++) gitVersion
