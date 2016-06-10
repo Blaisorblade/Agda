69e5bcb0a7d57aa7aa623d1c63bdd661f430c081
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2016-06-10 16:46:54 +0200

    Include git-describe output in version message
    This complete version is used in --version and --help.
    Fix #2023.

--- src/full/Agda/Version.hs
+++ src/full/Agda/Version.hs
@@ -1,12 +1,31 @@
+{-# LANGUAGE TemplateHaskell #-}
 module Agda.Version where
 
+import Data.Maybe (maybe)
 import Data.Version
 import Data.List
 
+import Agda.Utils.GitRevision
+import Language.Haskell.TH.Syntax
 import qualified Paths_Agda as PA
 
 -- | The version of Agda.
 
+gitVersion :: Maybe String
+gitVersion = $(do
+  v <- qRunIO gitDescribe
+  lift $ v)
+
 version :: String
-version = intercalate "." $ map show $
-            versionBranch PA.version
+version = (intercalate "." $ map show $
+            versionBranch PA.version)
+
+fullVersion :: String
+fullVersion =
+  case gitVersion of
+    Nothing -> version
+    Just ver ->
+      if version `isPrefixOf` ver then
+        ver
+      else
+        version ++ ". Git version: " ++ ver
